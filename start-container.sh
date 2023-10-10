ab59c8fbc4cef2570eafa75875a3997e7a167e1f
Brandt Hill <brandtlarsonhill@gmail.com>
2023-10-10 17:34:00 -0500

    script fix

--- start-container.sh
+++ start-container.sh
@@ -4,6 +4,6 @@ docker stop djbot
 
 docker rm djbot
 
-sed -e 's/^export //g' -e '/"//g' .env > .env.docker
+sed -e 's/^export //g' -e 's/"//g' .env > .env.docker
 
 docker run -d --log-opt max-size=5m --network host --volume /home/brandt/music:/mus --env-file .env.docker --restart always --name djbot brandt/djbot
