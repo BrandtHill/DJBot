7162ad421425221d32aa41892ca336bc6c2ab3f7
Brandt Hill <brandtlarsonhill@gmail.com>
2023-10-10 17:26:40 -0500

    docker updates

--- config/releases.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-config :nostrum,
-  token: System.get_env("DISCORD_API_KEY")
-
-config :logger, :console, metadata: [:shard, :guild, :channel]
--- start-image.sh
+++ start-image.sh
@@ -6,4 +6,4 @@ docker rm djbot
 
 sed -e 's/^export //g' -e '/"//g' .env > .env.docker
 
-docker run -d --log-opt max-size=5m --network host --env-file .env.docker --restart always --name djbot brandt/djbot
+docker run -d --log-opt max-size=5m --network host --volume /home/brandt/music:/mus --env-file .env.docker --restart always --name djbot brandt/djbot
